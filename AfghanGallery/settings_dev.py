9cfa94ab3f592c1f7cde429fd4ed2f5ea1f22b6d
Masoom-Wahid <masoom3892@gmail.com>
2024-08-16 17:13:48 +0430

    Fix  : CSRF issue

--- AfghanGallery/settings_dev.py
+++ AfghanGallery/settings_dev.py
@@ -3,6 +3,9 @@ import os
 JWT_EXPIRATION_TIME = 172800
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
+CSRF_TRUSTED_ORIGINS = [
+    'https://afghangallery.mehdiwahid.dev'
+]
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
 DATABASES = {
