19a1616294785e410fff8c15c2242ad6fe4c798a
Masoom-Wahid <masoom3892@gmail.com>
2024-09-03 23:23:49 +0430

    testing the new ci/cd (3)

--- AfghanGallery/urls.py
+++ AfghanGallery/urls.py
@@ -24,7 +24,7 @@ router.register(r'payments',PaymentPlanViewSet,basename="payment")
 schema_view = get_schema_view(
    openapi.Info(
       title="AfghanGallery API",
-      default_version='v3',
+      default_version='v4',
       description="Description",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
