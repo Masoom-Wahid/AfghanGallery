9f800d8a9fe4daa707d2113ce1090ce38d768611
Masoom-Wahid <masoom3892@gmail.com>
2024-09-18 11:45:21 +0430

    Feat : rewrote the chat in socket.io

--- chat/utils.go
+++ chat/utils.go
@@ -3,13 +3,72 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/go-redis/redis/v8"
 )
 
 var redisClient *redis.Client
 
+type JoinArgs struct {
+	OtherEmail string `json:"email"`
+}
+type MsgArgs struct {
+	Receiver int    `json:"receiver"`
+	Msg      string `json:"msg"`
+}
+
+type RoomArgs struct {
+	Room string `json:"room"`
+}
+
+func get_user_from_ctx(user_ctx string) (User, error) {
+	user := strings.Fields(user_ctx)
+	if len(user) < 3 {
+		return User{}, fmt.Errorf("invalid input format")
+	}
+
+	id, err := strconv.Atoi(user[0])
+	if err != nil {
+		return User{}, fmt.Errorf("failed to parse ID: %v", err)
+	}
+
+	is_verified, err := strconv.ParseBool(user[2])
+	if err != nil {
+		return User{}, fmt.Errorf("failed to parse is_verified: %v", err)
+	}
+
+	return User{
+		ID:       id,
+		Email:    user[1],
+		Verified: is_verified,
+	}, nil
+}
+
+func del_room(roomID int, user User) {
+	if room, exists := connections[user.ID]; exists {
+		if roomID == room && room != 0 {
+			connections[user.ID] = 0
+		}
+	}
+}
+
+func save_room_state(room string, user User) {
+	roomMutex.Lock()
+	defer roomMutex.Unlock()
+	roomConnections[room]--
+	roomID, err := strconv.Atoi(room)
+	if err != nil {
+		return
+	}
+	del_room(roomID, user)
+	if roomConnections[room] <= 0 {
+		saveMessagesToDB(room)
+		redisClient.Del(ctx, "chat:"+room)
+		delete(roomConnections, room)
+	}
+}
 func InitRedis(redis_host, redis_port, redis_password string) {
 	redis_addr := fmt.Sprintf("%s:%s", redis_host, redis_port)
 	redisClient = redis.NewClient(&redis.Options{
@@ -41,24 +100,4 @@ func Init(
 		DB_path,
 	)
 	println("Database initiaed")
-}
-
-func writeResponse(from string, detail string, _ http.ResponseWriter) {
-	// Cant write response
-	if debug == "TRUE" {
-		fmt.Printf("here with from %s\nand detail %s\n", from, detail)
-	}
-	// response := map[string]string{
-	// 	"from":   from,
-	// 	"detail": detail,
-	// }
-
-	// // Set the status code and content type
-	// w.WriteHeader(http.StatusBadRequest)
-	// w.Header().Set("Content-Type", "application/json")
-
-	// // Write the JSON response
-	// if err := json.NewEncoder(w).Encode(response); err != nil {
-	// 	log.Printf("Failed to write JSON response: %v", err)
-	// }
 }
